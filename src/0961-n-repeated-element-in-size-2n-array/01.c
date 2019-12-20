5ac154a178ccc9333fb19ff370f7de2955d14141
hing1 <hing123@126.com>
2019-12-20 19:58:50 +0800

    create 0961---01.c

---
+++ src/0961-n-repeated-element-in-size-2n-array/01.c
@@ -0,0 +1,26 @@
+/*************************************************************************
+# File Name: 01.c
+# Author: Hing
+# mail: hing123@126.com
+# Created Time: Fri Dec 20 19:27:51 2019
+ ************************************************************************/
+
+int repeatedNTimes(int* A, int ASize){
+    int num[10000];
+    int counts = 0;
+    
+    num[0] = A[0];
+    counts++;
+    
+    for(int i = 1; i < ASize; i++) {
+        int j;
+        for(j = 0;j < counts;j++) {
+            if(A[i] == num[j])
+                return A[i];
+        }
+        num[j] = A[i];
+        counts++;
+    }
+    return -1;
+}
+
