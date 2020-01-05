a342d1a55775015e1f57007ea2ec6db4a98b83bc
hing1 <hing123@126.com>
2020-01-05 20:18:26 +0800

    create 1108-01.cpp

---
+++ src/1108-defanging-an-ip-address/01.cpp
@@ -0,0 +1,21 @@
+/*************************************************************************
+# File Name: 01.cpp
+# Author: Hing
+# mail: hing123@126.com
+# Created Time: Sun Jan  5 20:15:11 2020
+ ************************************************************************/
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string s = address;
+        for (int i = 0, count = 0; i < address.size(); ++i) {
+            if (address.at(i) == '.') {
+                s.insert(i + count * 2, "[");
+                s.insert(i + 2 + count * 2, "]");
+                ++count;
+            }
+        }
+        return s;
+    }
+};
+
