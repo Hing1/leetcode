b9d5aaf90bd60f3b64298882d5923caa6a678c5b
hing1 <hing123@126.com>
2020-01-06 20:18:09 +0800

    create 344-01.c

---
+++ src/0344-reverse-string/01.c
@@ -0,0 +1,17 @@
+/*************************************************************************
+# File Name: 01.c
+# Author: Hing
+# mail: hing123@126.com
+# Created Time: Mon Jan  6 20:16:23 2020
+ ************************************************************************/
+
+void reverseString(char* s, int sSize) {
+    char temp;
+    int n = sSize / 2;
+    for(int i = 0;i < n;i++){
+        temp = s[i];
+        s[i] = s[sSize - 1 - i];
+        s[sSize - 1 - i] = temp;
+    }
+}
+
