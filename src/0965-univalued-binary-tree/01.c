e7c47a75b311ab945acd9afca03b6271c02cf8d5
hing1 <hing123@126.com>
2020-01-06 20:29:42 +0800

    create 965-01.c

---
+++ src/0965-univalued-binary-tree/01.c
@@ -0,0 +1,33 @@
+/*************************************************************************
+# File Name: 01.c
+# Author: Hing
+# mail: hing123@126.com
+# Created Time: Mon Jan  6 20:27:36 2020
+ ************************************************************************/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+bool isUnivalTree(struct TreeNode* root){
+    if (root == NULL)
+        return true;
+    if (root->left != NULL && root->right != NULL) {
+        if (root->val != root->left->val || root->val != root->right->val)
+            return false;
+    } else if (root->left != NULL) {
+        if (root->val != root->left->val)
+            return false;
+    } else if (root->right != NULL) {
+        if (root->val != root->right->val)
+            return false;
+    }
+    return (isUnivalTree(root->left) && isUnivalTree(root->right));
+}
+
