d207b05b267ebe34124a8a7e3e214a8eeb4c246d
hing1 <hing123@126.com>
2020-01-06 20:08:23 +0800

    create 0226-01.c

---
+++ src/0226-invert-binary-tree/01.c
@@ -0,0 +1,27 @@
+/*************************************************************************
+# File Name: 01.c
+# Author: Hing
+# mail: hing123@126.com
+# Created Time: Mon Jan  6 20:07:04 2020
+ ************************************************************************/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+struct TreeNode* invertTree(struct TreeNode* root){
+    if (root == NULL)
+        return NULL;
+    struct TreeNode *t = (struct TreeNode *)malloc(sizeof(struct TreeNode));
+    t->val = root->val;
+    t->left = invertTree(root->right);
+    t->right = invertTree(root->left);
+    return t;
+}
+
