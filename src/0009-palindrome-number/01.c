eaba9bff34d525ccfd8f5d224b12701fadf85bd3
hing1 <hing123@126.com>
2020-01-07 13:38:00 +0800

    create 9-01.c

---
+++ src/0009-palindrome-number/01.c
@@ -0,0 +1,22 @@
+/*************************************************************************
+# File Name: 01.c
+# Author: Hing
+# mail: hing123@126.com
+# Created Time: Tue Jan  7 13:30:05 2020
+ ************************************************************************/
+
+unsigned int reverse(int n) {
+    unsigned int rev = 0;
+    while (n) {
+        rev = rev * 10 + n % 10;
+        n /= 10;
+    }
+    return rev;
+}
+bool isPalindrome(int x){
+    if (x < 0)
+        return false;
+    unsigned int rev = reverse(x);
+    return (rev == x);
+}
+
