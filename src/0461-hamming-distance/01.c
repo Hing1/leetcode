7fd7795e14e8b8be3b40c96a06af83204d8f0d70
hing1 <hing123@126.com>
2020-01-07 13:47:15 +0800

    create 461-01.c

---
+++ src/0461-hamming-distance/01.c
@@ -0,0 +1,30 @@
+/*************************************************************************
+# File Name: 01.c
+# Author: Hing
+# mail: hing123@126.com
+# Created Time: Tue Jan  7 13:46:31 2020
+ ************************************************************************/
+
+int hammingDistance(int x, int y) {
+    int a[32] = {0};
+    int b[32] = {0};
+    int na = 0,nb = 0;
+    int i,num = 0;
+    
+    while(x > 0){
+        a[na] = x % 2;
+        x = x / 2;
+        na++;
+    }
+    while(y > 0){
+        b[nb] = y % 2;
+        y = y / 2;
+        nb++;
+    }
+    
+    for(i = 0;i < 32;i++)
+        if(a[i] != b[i])
+            num++;
+    return num;
+}
+
