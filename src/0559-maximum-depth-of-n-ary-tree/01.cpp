fd931ce441b2c72e83cc5ad1550898c7629d9a92
hing1 <hing123@126.com>
2020-01-07 14:48:05 +0800

    create 559-01.cpp

---
+++ src/0559-maximum-depth-of-n-ary-tree/01.cpp
@@ -0,0 +1,40 @@
+/*************************************************************************
+# File Name: 01.cpp
+# Author: Hing
+# mail: hing123@126.com
+# Created Time: Tue Jan  7 14:43:02 2020
+ ************************************************************************/
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+public:
+    int maxDepth(Node* root) {
+        int height = 0;
+        if (root != NULL) {
+            vector<Node *> child = root->children;
+            for (auto itr = root->children.begin(); itr != root->children.end(); ++itr)
+                height = max(height, maxDepth(*itr));
+            ++height;
+        }
+        return height;
+    }
+};
+
