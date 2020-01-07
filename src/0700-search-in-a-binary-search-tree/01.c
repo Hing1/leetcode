bfbcfd352de3efd093b1027cc1c359cde7ece0d5
hing1 <hing123@126.com>
2020-01-07 16:37:11 +0800

    create 700-01.c

---
+++ src/0700-search-in-a-binary-search-tree/01.c
@@ -0,0 +1,27 @@
+/*************************************************************************
+# File Name: 01.c
+# Author: Hing
+# mail: hing123@126.com
+# Created Time: Tue Jan  7 16:33:20 2020
+ ************************************************************************/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+struct TreeNode* searchBST(struct TreeNode* root, int val){
+    if (root == NULL)
+        return NULL;
+    if (root->val == val)
+        return root;
+    struct TreeNode *temp = searchBST(root->left, val);
+    if (temp == NULL)
+        temp = searchBST(root->right, val);
+    return temp;
+}
