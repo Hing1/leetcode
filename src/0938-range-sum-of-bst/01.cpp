3e408db5293e45da4950d9b02ba60cd2b22c7b5b
hing1 <hing123@126.com>
2020-01-07 16:38:26 +0800

    create 938-01.cpp

---
+++ src/0938-range-sum-of-bst/01.cpp
@@ -0,0 +1,28 @@
+/*************************************************************************
+# File Name: 01.cpp
+# Author: Hing
+# mail: hing123@126.com
+# Created Time: Tue Jan  7 16:28:13 2020
+ ************************************************************************/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int L, int R) {
+        if (root == NULL)
+            return 0;
+        if (L <= root->val && root->val <= R)
+            return (root->val + rangeSumBST(root->left, L, R) + (rangeSumBST(root->right, L, R)));
+        else
+            return (0 + rangeSumBST(root->left, L, R) + (rangeSumBST(root->right, L, R)));
+    }
+};
+
