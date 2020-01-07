8491e06b4044dd17d08b3b04d42096ed925ec115
hing1 <hing123@126.com>
2020-01-07 13:58:36 +0800

    create 1295-01.cpp

---
+++ src/1295-find-numbers-with-even-number-of-digits/01.cpp
@@ -0,0 +1,27 @@
+/*************************************************************************
+# File Name: 01.cpp
+# Author: Hing
+# mail: hing123@126.com
+# Created Time: Tue Jan  7 13:57:21 2020
+ ************************************************************************/
+
+class Solution {
+public:
+    bool isEvenDigit(int n) {
+        int count = 0;
+        while (n) {
+            ++count;
+            n = n / 10;
+        }
+        return (count % 2 == 0);
+    }
+    int findNumbers(vector<int>& nums) {
+        int count = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (isEvenDigit(nums[i]))
+                ++count;
+        }
+        return count;
+    }
+};
+
