1c45a2d7a0d527f7f3b3a083b058719bb58349e3
hing1 <hing123@126.com>
2020-01-08 20:07:22 +0800

    create 0292-01.cpp

---
+++ src/0292-nim-game/01.cpp
@@ -0,0 +1,20 @@
+/*************************************************************************
+# File Name: 01.cpp
+# Author: Hing
+# mail: hing123@126.com
+# Created Time: Wed Jan  8 20:05:38 2020
+ ************************************************************************/
+
+class Solution {
+public:
+    bool canWinNim(int n) {
+#if 0
+        if (n == 1 || n == 2 || n == 3)
+            return true;
+        if (n == 4)
+            return false;
+        return !(canWinNim(n - 1) && canWinNim(n - 2) && canWinNim(n - 3));
+#endif
+        return n & 3;
+    }
+};
