e680e2a09fd5ad7c9b6487e52bd223fcd93c8939
hing1 <hing123@126.com>
2020-01-10 18:25:41 +0800

    create 1047-01.cpp

---
+++ src/1047-remove-all-adjacent-duplicates-in-string/01.cpp
@@ -0,0 +1,25 @@
+/*************************************************************************
+# File Name: 01.cpp
+# Author: Hing
+# mail: hing123@126.com
+# Created Time: Fri Jan 10 08:48:12 2020
+ ************************************************************************/
+
+class Solution {
+public:
+    string removeDuplicates(string S) {
+        stack<char> stk;
+        for (int i = S.size() - 1; i >= 0; --i) {
+            if (stk.empty() || stk.top() != S[i])
+                stk.push(S[i]);
+            else
+                stk.pop();
+        }
+        string ans = "";
+        while (!stk.empty()) {
+            ans += stk.top();
+            stk.pop();
+        }
+        return ans;
+    }
+};
