6e620d27502549bd50954948f97180a6daa50e1b
hing1 <hing123@126.com>
2020-01-11 08:51:02 +0800

    create 108-02.c

---
+++ src/0108-convert-sorted-array-to-binary-search-tree/02.c
@@ -0,0 +1,29 @@
+/*************************************************************************
+# File Name: 02.c
+# Author: Hing
+# mail: hing123@126.com
+# Created Time: Sat Jan 11 08:49:09 2020
+ ************************************************************************/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+struct TreeNode * mySort(int *nums, int left, int right) {
+    if (left >= right)
+        return NULL;
+    int mid = (left + right) / 2;
+    struct TreeNode *root = (struct TreeNode *) malloc (sizeof(struct TreeNode));
+    root->val = nums[mid];
+    root->left = mySort(nums, left, mid);
+    root->right = mySort(nums, mid + 1, right);
+    return root;
+}
+struct TreeNode* sortedArrayToBST(int* nums, int numsSize){
+    return mySort(nums, 0, numsSize);
+}
