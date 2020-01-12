6c35a4164cd337e9bc0312b4e414789cdd7e088a
hing1 <hing123@126.com>
2020-01-12 15:34:39 +0800

    create 0908-01.cpp

---
+++ src/0908-smallest-range-i/01.cpp
@@ -0,0 +1,22 @@
+/*************************************************************************
+# File Name: 01.cpp
+# Author: Hing
+# mail: hing123@126.com
+# Created Time: Sun Jan 12 15:33:11 2020
+ ************************************************************************/
+
+class Solution {
+public:
+    int smallestRangeI(vector<int>& A, int K) {
+        int minnumb = A[0];
+        int maxnumb = A[0];
+        for (auto &i:A) {
+            maxnumb = max (i, maxnumb);
+            minnumb = min (i, minnumb);
+        }
+        if (maxnumb - K <= minnumb + K)
+            return 0;
+        else
+            return (maxnumb - minnumb - 2 * K);
+    }
+};
