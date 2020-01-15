9c167e98ae66a730876da684c19caef06b515b99
hing1 <hing123@126.com>
2020-01-15 23:49:47 +0800

    create 821-01.cpp

---
+++ src/0821-shortest-distance-to-a-character/01.cpp
@@ -0,0 +1,31 @@
+/*************************************************************************
+# File Name: 01.cpp
+# Author: Hing
+# mail: hing123@126.com
+# Created Time: Wed Jan 15 21:44:13 2020
+ ************************************************************************/
+
+class Solution {
+public:
+    int my_distance(string str, char key, int idx) {
+        int i = 0;
+        while (1) {
+            if (idx + i < str.size()) {
+                if (str[idx + i] == key)
+                    return i;
+            }
+            if (idx - i >= 0) {
+                if (str[idx - i] == key)
+                    return i;
+            }
+            ++i;
+        }
+    }
+    vector<int> shortestToChar(string S, char C) {
+        vector<int> vi;
+        for (int i = 0; i < S.size(); ++i) {
+            vi.push_back(my_distance(S, C, i));
+        }
+        return vi;
+    }
+};
