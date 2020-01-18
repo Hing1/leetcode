ebab96297c28d4e731c88d845d1c4af8f35baea1
hing1 <hing123@126.com>
2020-01-18 09:12:07 +0800

    create 897-01.cpp

---
+++ src/0897-increasing-order-search-tree/01.cpp
@@ -0,0 +1,55 @@
+/*************************************************************************
+# File Name: 01.cpp
+# Author: Hing
+# mail: hing123@126.com
+# Created Time: Sat Jan 18 09:08:56 2020
+ ************************************************************************/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> traversal(TreeNode *root) {
+        vector<int> vi;
+        if (root != NULL) {
+            vector<int> temp = traversal(root->left);
+            for (auto &i:temp)
+                vi.push_back(i);
+            vi.push_back(root->val);
+            temp = traversal(root->right);
+            for (auto &i:temp)
+                vi.push_back(i);
+            temp.clear();
+        }
+        return vi;
+    }
+    void insert(TreeNode *&root, int key) {
+        TreeNode *temp = new TreeNode(key);
+        if (root == NULL) {
+            root = temp;
+        } else {
+            TreeNode *tRoot = root;
+            while(tRoot->right != NULL)
+                tRoot = tRoot->right;
+            tRoot->right = temp;
+        }
+    }
+    TreeNode* increasingBST(TreeNode* root) {
+        vector<int> vi;
+        if (vi.size() == 0)
+            vi = traversal(root);
+        TreeNode *ans = NULL;
+        for (int i = 0; i < vi.size(); ++i) {
+            insert(ans, vi[i]);
+        }
+        vi.clear();
+        return ans;
+    }
+};
