6c4fc4e85c85b565597a9450d206015747c5741a
hing1 <hing123@126.com>
2020-01-27 13:23:47 +0800

    create 1323-01.cpp

---
+++ src/1323-maximum-69-number/01.cpp
@@ -0,0 +1,28 @@
+/*************************************************************************
+# File Name: 01.cpp
+# Author: Hing
+# mail: hing123@126.com
+# Created Time: Mon Jan 27 11:54:29 2020
+ ************************************************************************/
+
+class Solution {
+public:
+    int maximum69Number (int num) {
+        vector<int> vi;
+        while (num) {
+            vi.push_back(num % 10);
+            num /= 10;
+        }
+        for (int i = vi.size() - 1; i >= 0; --i) {
+            if (vi[i] == 6) {
+                vi[i] = 9;
+                break;
+            }
+        }
+        int ans = 0;
+        for (int i = vi.size() - 1; i >= 0; --i) {
+            ans = ans * 10 + vi[i];
+        }
+        return ans;
+    }
+};
