23e38c0b1f1a66fa1c89cece98cba66c9d595575
hing1 <hing123@126.com>
2020-01-27 13:29:23 +0800

    create 1309-01.cpp

---
+++ src/1309-decrypt-string-from-alphabet-to-integer-mapping/01.cpp
@@ -0,0 +1,28 @@
+/*************************************************************************
+# File Name: 01.cpp
+# Author: Hing
+# mail: hing123@126.com
+# Created Time: Mon Jan 27 13:07:24 2020
+ ************************************************************************/
+
+class Solution {
+public:
+    string freqAlphabets(string s) {
+        string re_ans;
+        int dif_char = 'a' - '1';
+        int dif_int = '1' - 1;
+        for (int i = s.size() - 1; i >= 0; --i) {
+            if ('0' <= s[i] && s[i] <= '9') {
+                re_ans += s[i] + dif_char;
+            } else {
+                int temp = (s[--i] - dif_int) + (s[--i] - dif_int) * 10;
+                re_ans += temp + dif_char + dif_int;
+            }
+        }
+        string ans;
+        for (int i = re_ans.size() - 1; i >= 0; --i) {
+            ans += re_ans[i];
+        }
+        return ans;
+    }
+};
