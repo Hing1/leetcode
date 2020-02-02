ba4c2ec26d5a71c0e0be18cdfa73b9d9fe3da59d
hing1 <hing123@126.com>
2020-02-02 10:37:40 +0800

    create 0027-01.cpp

---
+++ src/0027-remove-element/01.cpp
@@ -0,0 +1,23 @@
+/*************************************************************************
+# File Name: 01.cpp
+# Author: Hing
+# Mail: hing123@126.com
+# Created Time: Sun Feb  2 10:35:30 2020
+ ************************************************************************/
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int back = nums.size() - 1;
+        int i = 0;
+        while (i <= back) {
+            if (nums[i] == val) {
+                nums[i] = nums[back];
+                back--;
+            } else {
+                ++i;
+            }
+        }
+        return back + 1;
+    }
+};
