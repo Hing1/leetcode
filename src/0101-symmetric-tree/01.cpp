9d1332de4091d0b32ef6c9d8973328c995d670f6
hing1 <hing123@126.com>
2020-02-04 23:26:56 +0800

    create 101-01.cpp

---
+++ src/0101-symmetric-tree/01.cpp
@@ -0,0 +1,35 @@
+/*************************************************************************
+# File Name: 01.cpp
+# Author: Hing
+# Mail: hing123@126.com
+# Created Time: Tue Feb  4 23:24:39 2020
+ ************************************************************************/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isMirror(TreeNode *r1, TreeNode *r2) {
+        if (r1 == NULL && r2 == NULL) {
+            return true;
+        } else if (r1 == NULL || r2 == NULL) {
+            return false;
+        } else {
+            if (r1->val != r2->val)
+                return false;
+            return isMirror(r1->left, r2->right) && isMirror(r1->right, r2->left);
+        }
+    }
+    bool isSymmetric(TreeNode* root) {
+        if (NULL == root)
+            return true;
+        return isMirror(root->left, root->right);
+    }
+};
