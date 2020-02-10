392d1fa782e4e370d59061feff48376649ca7328
hing1 <hing123@126.com>
2020-02-10 15:36:18 +0800

    create 189-01.cpp

---
+++ src/0189-rotate-array/01.cpp
@@ -0,0 +1,24 @@
+/*************************************************************************
+# File Name: 01.cpp
+# Author: Hing
+# Mail: hing123@126.com
+# Created Time: Mon Feb 10 15:34:42 2020
+ ************************************************************************/
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k = k % nums.size();
+        vector<int> vi;
+        vi.reserve(k);
+        for (int i = nums.size() - k; i < nums.size(); ++i) {
+            vi.push_back(nums[i]);
+        }
+        for (int i = nums.size() - 1; i >= k; --i) {
+            nums[i] = nums[i - k];
+        }
+        for (int i = 0; i < k; ++i) {
+            nums[i] = vi[i];
+        }
+    }
+};
