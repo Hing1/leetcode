34bcf0b23a8e4203460ede7de33452381770b502
hing1 <hing123@126.com>
2020-02-12 21:37:45 +0800

    create 118-01.cpp

---
+++ src/0118-pascals-triangle/01.cpp
@@ -0,0 +1,31 @@
+/*************************************************************************
+# File Name: 01.cpp
+# Author: Hing
+# Mail: hing123@126.com
+# Created Time: Wed Feb 12 21:36:20 2020
+ ************************************************************************/
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        if (numRows <= 0) {
+            vector<vector<int>> ans;
+            return ans;
+        } else if (numRows == 1) {
+            vector<int> vi = {1};
+            vector<vector<int>> ans;
+            ans.push_back(vi);
+            return ans;
+        } else {
+            vector<vector<int>> ans = generate(numRows - 1);
+            vector<int> pre = ans.back();
+            vector<int> temp = {1};
+            for (int i = 0; i < pre.size() - 1; ++i) {
+                temp.push_back(pre[i] + pre[i + 1]);
+            }
+            temp.push_back(1);
+            ans.push_back(temp);
+            return ans;
+        }
+    }
+};
