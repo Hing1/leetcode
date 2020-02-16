3e7bd22e72d78332b8464eedab4d812c0b8aeef8
hing1 <hing123@126.com>
2020-02-17 01:26:17 +0800

    create 26-01.cpp

---
+++ src/0026-remove-duplicates-from-sorted-array/01.cpp
@@ -0,0 +1,27 @@
+/*************************************************************************
+# File Name: 01.cpp
+# Author: Hing
+# Mail: hing123@126.com
+# Created Time: Mon Feb 17 00:27:00 2020
+ ************************************************************************/
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.size() <= 1)
+            return nums.size();
+        int count = 0;
+        for (int i = 1; i < nums.size(); ++i) {
+            while (i < nums.size() && nums[count] == nums[i]) {
+                ++i;
+            }
+            if (i == nums.size()) {
+                break;
+            } else {
+                ++count;
+                nums[count] = nums[i];
+            }
+        }
+        return count + 1;
+    }
+};
