942aa47e3a07d4f658ff255b31e785bf1fe61f72
hing1 <hing123@126.com>
2020-02-17 01:25:08 +0800

    create 14-01.cpp

---
+++ src/0014-longest-common-prefix/01.cpp
@@ -0,0 +1,34 @@
+/*************************************************************************
+# File Name: 01.cpp
+# Author: Hing
+# Mail: hing123@126.com
+# Created Time: Mon Feb 17 00:10:45 2020
+ ************************************************************************/
+
+class Solution {
+public:
+    string lcp_of_two_str(string str1, string str2) {
+        if (str1.size() == 0 || str2.size() == 0)
+            return "";
+        string ans = "";
+        for (int i = 0; i < str1.size() && i < str2.size(); ++i) {
+            if (str1[i] == str2[i])
+                ans+=str1[i];
+            else
+                break;
+        }
+        return ans;
+    }
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.size() == 0) {
+            return "";
+        }
+        string ans = strs[0];
+        if (strs.size() == 1)
+            return ans;
+        for (int i = 1; i < strs.size(); ++i) {
+            ans = lcp_of_two_str(ans, strs[i]);
+        }
+        return ans;
+    }
+};
