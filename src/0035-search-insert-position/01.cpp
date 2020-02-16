21f3da7b8d4b666708d6246cd8f0f235098b8fb2
hing1 <hing123@126.com>
2020-02-17 01:29:39 +0800

    create 0035-01.cpp

---
+++ src/0035-search-insert-position/01.cpp
@@ -0,0 +1,25 @@
+/*************************************************************************
+# File Name: 01.cpp
+# Author: Hing
+# Mail: hing123@126.com
+# Created Time: Mon Feb 17 01:05:41 2020
+ ************************************************************************/
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
