a67a90a8ef74c6626edf6865124b6dde25d2a5cd
hing1 <hing123@126.com>
2020-02-17 01:28:12 +0800

    create 28-01.cpp

---
+++ src/0028-implement-strstr/01.cpp
@@ -0,0 +1,26 @@
+/*************************************************************************
+# File Name: 01.cpp
+# Author: Hing
+# Mail: hing123@126.com
+# Created Time: Mon Feb 17 00:35:57 2020
+ ************************************************************************/
+
+class Solution {
+public:
+    bool same_begin(string str, int idx, string key) {
+        for (int i = 0; i < key.size(); ++i) {
+            if (str[i + idx] != key[i])
+                return false;
+        }
+        return true;
+    }
+    int strStr(string haystack, string needle) {
+        if (needle.size() == 0)
+            return 0;
+        for (int i = 0; i < haystack.size(); ++i) {
+            if (same_begin(haystack, i, needle))
+                return i;
+        }
+        return -1;
+    }
+};
