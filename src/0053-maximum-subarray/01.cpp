6b8f1fee1773e0c91683db804073d8613616a7a2
hing1 <hing123@126.com>
2020-02-19 01:06:02 +0800

    create 53-01.cpp

---
+++ src/0053-maximum-subarray/01.cpp
@@ -0,0 +1,22 @@
+/*************************************************************************
+# File Name: 01.cpp
+# Author: Hing
+# Mail: hing123@126.com
+# Created Time: Wed Feb 19 01:03:29 2020
+ ************************************************************************/
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int ans = nums[0];
+        int temp = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (temp > 0)
+                temp += nums[i];
+            else
+                temp = nums[i];
+            ans = max(ans, temp);
+        }
+        return ans;
+    }
+};
