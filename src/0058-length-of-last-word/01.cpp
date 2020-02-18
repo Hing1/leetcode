c7f57a45288bec2913c87cbd5bcaa5ecb08928af
hing1 <hing123@126.com>
2020-02-18 23:44:41 +0800

    create 58-01.cpp

---
+++ src/0058-length-of-last-word/01.cpp
@@ -0,0 +1,21 @@
+/*************************************************************************
+# File Name: 01.cpp
+# Author: Hing
+# Mail: hing123@126.com
+# Created Time: Tue Feb 18 23:42:03 2020
+ ************************************************************************/
+
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int ans = 0;
+        int i = s.size() - 1;
+        while (s[i] == ' ')
+            --i;
+        while (i >= 0 && s[i] != ' ') {
+            --i;
+            ++ans;
+        }
+        return ans;
+    }
+};
