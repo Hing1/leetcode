7f3021c6a6e8c382b15b6908060db61263595fa7
hing1 <hing123@126.com>
2020-02-20 22:36:31 +0800

    create 762-01.cpp

---
+++ src/0762-prime-number-of-set-bits-in-binary-representation/01.cpp
@@ -0,0 +1,40 @@
+/*************************************************************************
+# File Name: 01.cpp
+# Author: Hing
+# Mail: hing123@126.com
+# Created Time: Thu Feb 20 22:34:37 2020
+ ************************************************************************/
+
+class Solution {
+public:
+    int setBit(int n) {
+        int count = 0;
+        while (n) {
+            if (n % 2)
+                ++count;
+            n /= 2;
+        }
+        return count;
+    }
+    bool isPrime(int n) {
+        if (n == 1)
+            return false;
+        bool flag = true;
+        for(int i = 2; i <= n / 2; ++i) {
+            if(n % i == 0) {
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
+    int countPrimeSetBits(int L, int R) {
+        int count = 0;
+        for (int i = L; i <= R; ++i) {
+            int temp = setBit(i);
+            if (isPrime(temp))
+                ++count;
+        }
+        return count;
+    }
+};
