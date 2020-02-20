f56e80e40a53e3e9d3b270d50bc81767613dd7b6
hing1 <hing123@126.com>
2020-02-20 18:35:10 +0800

    create 70-01.cpp

---
+++ src/0070-climbing-stairs/01.cpp
@@ -0,0 +1,21 @@
+/*************************************************************************
+# File Name: 01.cpp
+# Author: Hing
+# Mail: hing123@126.com
+# Created Time: Thu Feb 20 18:22:41 2020
+ ************************************************************************/
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n <= 0)
+            return 1;
+        vector<int> vi(n + 1, 0);
+        vi[0] = 1;
+        vi[1] = 1;
+        for (int i = 2; i <= n; ++i) {
+            vi[i] = vi[i - 1] + vi[i - 2];
+        }
+        return vi[n];
+    }
+};
