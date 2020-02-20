776b0c56c14e7ce0e46f99bce904da5440209639
hing1 <hing123@126.com>
2020-02-20 22:02:19 +0800

    create 111-01.cpp

---
+++ src/0111-minimum-depth-of-binary-tree/01.cpp
@@ -0,0 +1,31 @@
+/*************************************************************************
+# File Name: 01.cpp
+# Author: Hing
+# Mail: hing123@126.com
+# Created Time: Thu Feb 20 21:59:19 2020
+ ************************************************************************/
+af
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if (root == NULL)
+            return 0;
+        if (root->left == NULL && root->right == NULL)
+            return 1;
+        if (root->left && root->right)
+            return min(minDepth(root->left), minDepth(root->right)) + 1;
+        if (root->left)
+            return minDepth(root->left) + 1;
+        else
+            return minDepth(root->right) + 1;
+    }
+};
