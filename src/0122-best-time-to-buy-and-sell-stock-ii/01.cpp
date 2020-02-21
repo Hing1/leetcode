ad287884aad486e9953490b1a8fe6b9683036a88
hing1 <hing123@126.com>
2020-02-21 09:53:09 +0800

    create 122-01.cpp

---
+++ src/0122-best-time-to-buy-and-sell-stock-ii/01.cpp
@@ -0,0 +1,25 @@
+/*************************************************************************
+# File Name: 01.cpp
+# Author: Hing
+# Mail: hing123@126.com
+# Created Time: Fri Feb 21 09:40:09 2020
+ ************************************************************************/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.size() <= 1)
+            return 0;
+        vector<int> vi;
+        vi.reserve(prices.size() - 1);
+        for (int i = 0; i < prices.size() - 1; ++i) {
+            vi.push_back(prices[i + 1] - prices[i]);
+        }
+        int ans = 0;
+        for (int i = 0; i < vi.size(); ++i) {
+            if (vi[i] > 0)
+                ans += vi[i];
+        }
+        return ans;
+    }
+};
