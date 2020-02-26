8df48fecbb2d4a1c4a3024c67d2e76b32ae37d45
hing1 <hing123@126.com>
2020-02-26 21:17:48 +0800

    create 278-01.cpp

---
+++ src/0278-first-bad-version/01.cpp
@@ -0,0 +1,25 @@
+/*************************************************************************
+# File Name: 01.cpp
+# Author: Kian Kwok
+# Mail: kiankwok6@gmail.com
+# Created Time: Wed Feb 26 21:15:27 2020
+ ************************************************************************/
+
+// Forward declaration of isBadVersion API.
+bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid))
+                right = mid;
+            else
+                left = mid + 1;
+        }
+        return left;
+    }
+};
