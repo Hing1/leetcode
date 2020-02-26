a6f1e0245ba29c1f615241d19fdafc3724371af1
hing1 <hing123@126.com>
2020-02-26 20:49:55 +0800

    create 263-01.cpp

---
+++ src/0263-ugly-number/01.cpp
@@ -0,0 +1,26 @@
+/*************************************************************************
+# File Name: 01.cpp
+# Author: Kian Kwok
+# Mail: kiankwok6@gmail.com
+# Created Time: Wed Feb 26 16:47:56 2020
+ ************************************************************************/
+
+class Solution {
+public:
+    bool isUgly(int num) {
+        if (num <= 0)
+            return false;
+        while (num != 1) {
+            if (num % 2 == 0) {
+                num /= 2;
+            } else if (num % 3 == 0) {
+                num /= 3;
+            } else if (num % 5 == 0) {
+                num /= 5;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+};
