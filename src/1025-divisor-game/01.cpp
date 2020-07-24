766abf5d21ed29eb041c885b0a95fe57c77d61eb
Kian <KianKwok6@gmail.com>
2020-07-24 19:56:41 +0800

    create 1025 - cpp

---
+++ src/1025-divisor-game/01.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+# File Name: 01.cpp
+# Author: Kian Kwok
+# Mail: kiankwok6@gmail.com
+# Created Time: Fri Jul 24 19:47:06 2020
+ ************************************************************************/
+
+class Solution {
+public:
+    bool divisorGame(int N) {
+        return N % 2 == 0;
+    }
+};
+
